ffe81c49a13636ef2b7c67f54dd0e111230c64cd
patrick ramge <patrick ramge>
2016-03-17 20:51:10 +0100

    de pdo codes lol

---
+++ ophalen.php
@@ -0,0 +1,31 @@
+<table>
+	<?php
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		try {
+			//Creating connection for mysql
+			$db = new PDO("mysql:host=$servername;dbname=opdracht", $username, $password);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$query = $db->prepare("SELECT * FROM phples");
+			if ($query->execute()){
+				$tabel = $query->fetchAll(PDO::FETCH_ASSOC);
+				foreach ($tabel as $row) {
+					echo "<tr>";
+					foreach ($row as $column) {
+						echo "<td>";
+						print_r($column);
+						echo "</td>";
+					}
+					echo "</tr>";
+				}
+				/* for ($i=0; $i<count($tabel); $i++){
+					echo "<tr><td>". $tabel[$i] ."</td></tr>";
+				} */
+			}
+
+		}catch(PDOException $e){
+			echo "<div class=\"alert alert-danger\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Fout!</strong> ".$e->getMessage()."</div>";
+		}
+	?>
+</table>
